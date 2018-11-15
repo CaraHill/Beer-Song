5a36f746aa89a684d4c20b7d29faa6b727e155bf
Cara Hill <cara@rabidtech.co.nz>
2018-11-16 11:48:58 +1300

    Third test passing.

--- beer-song.js
+++ beer-song.js
@@ -1,6 +1,10 @@
 const Beer = {
   nextBottleCount: function(bottleCount) {
-    return bottleCount - 1;
+    if(bottleCount == 0) {
+      return `99`;
+    } else {
+      return bottleCount - 1;
+    }
   },
 
   bottlePhrase: function(bottleCount) {
@@ -21,9 +25,21 @@ const Beer = {
     }
   },
 
+  shareOrBuy: function(bottleCount) {
+    if(bottleCount == 0) {
+      return `Go to the store and buy some more`;
+    } else {
+      return `Take ${this.bottlePronoun(bottleCount)} down and pass it around`;
+    }
+  },
+
+  capitalizeFirstLetter: function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+
   verse: function(bottleCount) {
-    return `${this.bottlePhrase(bottleCount)} of beer on the wall, ${this.bottlePhrase(bottleCount)} of beer.
-Take ${this.bottlePronoun(bottleCount)} down and pass it around, ${this.bottlePhrase(this.nextBottleCount(bottleCount))} of beer on the wall.
+    return `${this.capitalizeFirstLetter(this.bottlePhrase(bottleCount))} of beer on the wall, ${this.bottlePhrase(bottleCount)} of beer.
+${this.shareOrBuy(bottleCount)}, ${this.bottlePhrase(this.nextBottleCount(bottleCount))} of beer on the wall.
 `;
   }
 }
