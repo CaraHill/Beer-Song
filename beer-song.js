2d74a873a2fd0406f59e4ef61def5d74c8758364
Cara Hill <cara@rabidtech.co.nz>
2018-11-16 11:17:27 +1300

    First test passing.

---
+++ beer-song.js
@@ -0,0 +1,9 @@
+const Beer = {
+  verse: function (arg) {
+    return `8 bottles of beer on the wall, 8 bottles of beer.
+Take one down and pass it around, 7 bottles of beer on the wall.
+`;
+  }
+}
+
+export { Beer }
