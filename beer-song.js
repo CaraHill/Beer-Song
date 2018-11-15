c9fb51634a5d5427f4cad7fb4dea08c85c571f2a
Cara Hill <cara@rabidtech.co.nz>
2018-11-16 11:36:50 +1300

    Second test passing.

--- beer-song.js
+++ beer-song.js
@@ -1,7 +1,29 @@
 const Beer = {
-  verse: function (arg) {
-    return `8 bottles of beer on the wall, 8 bottles of beer.
-Take one down and pass it around, 7 bottles of beer on the wall.
+  nextBottleCount: function(bottleCount) {
+    return bottleCount - 1;
+  },
+
+  bottlePhrase: function(bottleCount) {
+    if(bottleCount == 1) {
+      return `${bottleCount} bottle`;
+    } else if(bottleCount == 0)
+      return `no more bottles`;
+    else {
+      return `${bottleCount} bottles`;
+    }
+  },
+
+  bottlePronoun: function(bottleCount) {
+    if(bottleCount == 1) {
+      return `it`;
+    } else {
+      return `one`;
+    }
+  },
+
+  verse: function(bottleCount) {
+    return `${this.bottlePhrase(bottleCount)} of beer on the wall, ${this.bottlePhrase(bottleCount)} of beer.
+Take ${this.bottlePronoun(bottleCount)} down and pass it around, ${this.bottlePhrase(this.nextBottleCount(bottleCount))} of beer on the wall.
 `;
   }
 }
